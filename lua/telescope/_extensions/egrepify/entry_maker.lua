522c94b5733e50d50bba683a853ffc69c5bc3ead
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2023-10-05 14:55:46 +0200

    wip

--- lua/telescope/_extensions/egrepify/entry_maker.lua
+++ lua/telescope/_extensions/egrepify/entry_maker.lua
@@ -31,7 +31,88 @@ local function num_width(num)
   return math.floor(math.log10(num) + 1)
 end
 
-local function line_display(entry, data, opts)
+local get_line_highlights = function(bufnr, root, lnum, lang)
+  local line = vim.api.nvim_buf_get_lines(bufnr, lnum, lnum + 1, false)[1]
+  local query = vim.treesitter.query.get(lang, "highlights")
+  local line_highlights = setmetatable({}, {
+    __index = function(t, k)
+      local obj = {}
+      rawset(t, k, obj)
+      return obj
+    end,
+  })
+  if query then
+    for id, node in query:iter_captures(root, bufnr, lnum, lnum + 1) do
+      local hl = "@" .. query.captures[id]
+      if hl and type(hl) ~= "number" then
+        local row1, col1, row2, col2 = node:range()
+
+        if row1 == row2 then
+          local row = row1 + 1
+
+          for index = col1, col2 do
+            line_highlights[row][index] = hl
+          end
+        else
+          local row = row1 + 1
+          for index = col1, #line[row] do
+            line_highlights[row][index] = hl
+          end
+
+          while row < row2 + 1 do
+            row = row + 1
+
+            for index = 0, #(line[row] or {}) do
+              line_highlights[row][index] = hl
+            end
+          end
+        end
+      end
+    end
+  end
+  return line_highlights
+end
+
+local ts_highlight_defs = function(path)
+  local lang = vim.filetype.match { filename = path }
+  -- check if buffer is opened
+  local bufnr
+  local buffers = vim.iter(vim.api.nvim_list_bufs()):filter(function(b)
+    return vim.api.nvim_buf_get_name(b) and vim.api.nvim_buf_is_loaded(b)
+  end)
+  if #buffers == 1 then
+    bufnr = buffers[1]
+  else
+    -- read buffer
+    -- local current_bufnr = vim.api.nvim_get_current_buf
+    -- if vim.bo[current_bufnr].filetype == "TelescopePrompt" then
+    -- local previewer =
+    -- end
+    bufnr = vim.fn.bufadd(path)
+    if vim.fn.bufload(bufnr) == 0 then
+      bufnr = nil
+    end
+  end
+  local root
+  if type(bufnr) == "number" then
+    local parser = vim.treesitter.get_parser(bufnr, lang)
+    root = parser:parse()[1]:root()
+  end
+  return setmetatable({ bufnr = bufnr, path = path, root = root, lang = lang }, {
+    __index = function(self, lnum)
+      if self.bufnr then
+        if not self[lnum] then
+          self[lnum] = get_line_highlights(self.bufnr, self.root, lnum, self.lang)
+        end
+        return self[lnum]
+      else
+        return {}
+      end
+    end,
+  })
+end
+
+local function line_display(entry, data, opts, ts_highlights)
   entry = entry or {}
   local file_devicon, devicon_hl
   if opts.title == false then
@@ -86,6 +167,15 @@ local function line_display(entry, data, opts)
   if lnum or col then
     begin = begin + 1
   end
+
+  if not ts_highlights[entry.path] then
+    ts_highlights[entry.path] = ts_highlight_defs(entry.path)
+  end
+
+  for index, hl in pairs(ts_highlights[entry.path][lnum]) do
+    highlights[#highlights + 1] = { { begin + index, begin + index + 1 }, hl }
+  end
+
   if not vim.tbl_isempty(data["submatches"]) then
     local matches = data["submatches"]
     for i = 1, #matches do
@@ -157,6 +247,7 @@ return function(opts)
     items = items,
   })
   opts.title_display = vim.F.if_nil(opts.title_display, title_display)
+  local ts_highlights = {}
 
   return function(stream)
     local json_line = vim.json.decode(stream)
@@ -193,7 +284,7 @@ return function(opts)
         }
 
         local display = function()
-          return line_display(entry, data, opts)
+          return line_display(entry, data, opts, ts_highlights)
         end
         entry.display = display
         return entry
